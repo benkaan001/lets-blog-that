c205128215cfa64c0a834a90f08a08f20a5d6975
Ben Kaan <benkaan001@gmail.com>
2021-11-11 15:36:57 -0600

    add/test helpers on homepage

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,8 +1,59 @@
 const router = require('express').Router();
+const sequelize = require('../config/connection');
+const { Post, User, Comment } = require('../models');
 
-router.get('/', (req,res) => {
-    res.render('homepage');
-});
+
+// Handlebars.js will automatically feed this into the main.handlebars and respond
+// with complete HTML file.
+// router.get('/', (req, res) => {
+//     res.render('homepage', {
+//       id: 1,
+//       post_url: 'https://handlebarsjs.com/guide/',
+//       title: 'Handlebars Docs',
+//       created_at: new Date(),
+//     //   vote_count: 10,
+//       comments: [{}, {}],
+//       user: {
+//         username: 'test_user'
+//       }
+//     });
+//   });
+
+router.get('/', (req, res) => {
+    Post.findAll({
+      attributes: [
+        'id',
+        'post_url',
+        'title',
+        'created_at'
+      ],
+      include: [
+        {
+          model: Comment,
+          attributes: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+          include: {
+            model: User,
+            attributes: ['username']
+          }
+        },
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        // pass a single post object into the homepage template
+        // console.log(dbPostData[0]);
+        const posts = dbPostData.map(post => post.get({ plain: true }));
+        // res.render('homepage', dbPostData[0].get({ plain: true }));
+        res.render('homepage', { posts });
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 
 
