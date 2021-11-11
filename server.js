ebed75fa53ca5c28a9cc0b99a539c3ae4f677aab
Ben Kaan <benkaan001@gmail.com>
2021-11-11 14:40:06 -0600

    test template engine setup

--- server.js
+++ server.js
@@ -7,11 +7,22 @@ const sequelize = require('./config/connection');
 // add path to make css availabe to the client
 const path = require('path');
 
+// add Handlebars
+
+const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
+
+
+
 const app = express();
 const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
+
+// add Handlebars
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
 
 
 //turn on routes
