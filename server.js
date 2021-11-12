b33ca1275e60b1d0105519682d63548e33ebe296
Ben Kaan <benkaan001@gmail.com>
2021-11-12 15:58:23 -0600

    add frontend js and  add eventlistener

--- server.js
+++ server.js
@@ -1,40 +1,38 @@
+
 // npx nodemon server.js
-
-const express = require('express');
-const routes = require ('./controllers');
-const sequelize = require('./config/connection');
-
 // add path to make css availabe to the client
 const path = require('path');
+const express = require('express');
+const routes = require('./controllers');
 
-// add Handlebars
-
+// add handlebars
 const exphbs = require('express-handlebars');
-const hbs = exphbs.create({});
+const hbs = exphbs.create({ });
 
 
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-app.use(express.json());
-app.use(express.urlencoded({extended: true }));
+const sequelize = require("./config/connection");
+
+
 
 // add Handlebars
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
 
-
-//turn on routes
-
-app.use(routes);
-
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
 // serve public files as static assets
-
 app.use(express.static(path.join(__dirname, 'public')));
 
-// turn on connection to db and server
 
+
+//turn on routes
+app.use(routes);
+
+// turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
     app.listen(PORT, () => console.log
     (`  \n---------------------\n Now listening on ${PORT} \n---------------------`));
