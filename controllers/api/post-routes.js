07209d9d1564abaed1caf99572c5f9475a512834
Ben Kaan <benkaan001@gmail.com>
2021-11-14 02:10:15 -0600

    create authguard middleware

--- controllers/api/post-routes.js
+++ controllers/api/post-routes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { User, Post, Comment } = require('../../models');
+const withAuth = require('../../utils/auth');
 
 router.get('/', (req,res) => {
     Post.findAll({
@@ -63,7 +64,7 @@ router.get('/:id', (req,res) => {
     });
 });
 
-router.post('/', (req,res) => {
+router.post('/', withAuth, (req,res) => {
     Post.create({
         title: req.body.title,
         post_url: req.body.post_url,
@@ -76,7 +77,7 @@ router.post('/', (req,res) => {
     });
 });
 
-router.put('/:id', (req,res) => {
+router.put('/:id', withAuth, (req,res) => {
     Post.update(
         {
             title: req.body.title
@@ -101,7 +102,7 @@ router.put('/:id', (req,res) => {
     });
 });
 
-router.delete('/:id', (req,res) => {
+router.delete('/:id', withAuth, (req,res) => {
     Post.destroy({
         where: {
             id: req.params.id
